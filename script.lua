be59d3536bf74f2586f448f7430c9c06da7b2c33
synolope <synolope@gmail.com>
2022-01-28 18:39:05 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -228,12 +228,6 @@ if game.PlaceId == 370731277 then
 		StealthToggle.Text = "<b>Stealth:</b> " .. tostring(getgenv().stealth)
 	end)
 
-	game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
-		if State == Enum.TeleportState.Started then
-			syn.queue_on_teleport("print('e') getgenv().stealth = " .. tostring(getgenv().stealth))
-		end
-	end)
-
 	local overlap = false
 
 	function LoadPlayer(player)
